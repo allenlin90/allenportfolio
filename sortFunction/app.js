9e4cea3eef72ad3513a5118b6335ab39cad94d70
allenlin90 <ht016746@gmail.com>
2020-12-29 19:50:23 +0700

    update features to sorting function

--- sortFunction/app.js
+++ sortFunction/app.js
@@ -5,5 +5,44 @@ window.onload = function () {
             // document.querySelector('.order_details').style.cssText = `display: flex; justify-content: space-between;`;
             document.querySelector('.delivery').style.cssText = `display: block`;
         }, 500);
-    }, 2000);
+    }, 1500);
+
+    const state = {
+        senderShow: true,
+        receiverShow: true
+    }
+
+    const sender = document.querySelector('#sender');
+    const receiver = document.querySelector('#receiver');
+    const collapse = document.querySelector('#collapse-btn');
+
+    document.querySelector('#sender').addEventListener('click', function () {
+        hideCardAndShowButton('receiverShow', 'senderShow', receiver, sender, this);
+        this.children[1].children[3].style.display = 'block';
+    });
+
+    document.querySelector('#receiver').addEventListener('click', function () {
+        hideCardAndShowButton('senderShow', 'receiverShow', sender, receiver, this);
+        this.children[1].children[3].style.display = 'block';
+    });
+
+    function hideCardAndShowButton(updateStateTo, currentObjState, partyToHide, partyToShow, jsNode) {
+        state[updateStateTo] = false;
+        partyToHide.style.cssText = 'display: none';
+        partyToShow.style.width = `80%`;
+
+        // show return button
+        collapse.style.display = 'block';
+        collapse.innerText = 'Return';
+        collapse.addEventListener('click', function () {
+            if (!state[updateStateTo] && state[currentObjState]) {
+                console.log(`${updateStateTo} is returned`)
+                state[updateStateTo] = true;
+                partyToHide.style.cssText = 'display: block';
+                partyToShow.style.width = '40%';
+                collapse.style.display = 'none';
+                jsNode.children[1].children[3].style.display = 'none';
+            }
+        });
+    }
 }

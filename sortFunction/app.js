e36b87db2a956fea0b2169ab90cc8159984383f1
allenlin90 <ht016746@gmail.com>
2021-01-15 16:46:05 +0700

    update sort function after login with state and navigation

--- sortFunction/app.js
+++ sortFunction/app.js
@@ -1,13 +1,6 @@
 window.onload = function () {
-    setTimeout(function () {
-        document.querySelector('.spinner').style.display = 'none';
-        setTimeout(function () {
-            document.querySelector('#delivery_details').style.cssText = `display: block`;
-            document.querySelector('.login_button').style.cssText = `display: block`;
-        }, 500);
-    }, 1500);
-
     const body = document.querySelector('body');
+    const spinner = document.querySelector('.spinner');
     const sender = document.querySelector('#sender');
     const receiver = document.querySelector('#receiver');
     const collapse = document.querySelector('#collapse-btn');
@@ -15,23 +8,38 @@ window.onload = function () {
     const loginForm = document.querySelector('.login_content');
     const loginBtn = document.querySelector('#driver-login');
     const subLogin = document.querySelector('#submitLogin');
-    const appContents = document.querySelector('.container');
+    const deliveryDetails = document.querySelector('#delivery_details');
     const loginBtnDiv = document.querySelector('.login_button');
     const deliveryImage = document.querySelector('#delivery_image');
+    const navigation = document.querySelector('.navigation');
+    const orderInfoBtn = document.querySelector('#order_info');
+    const instructionBtn = document.querySelector('#instruction');
+    const logoutBtn = document.querySelector('#logout');
 
     const state = {
         senderShow: true,
         receiverShow: true,
-        loggedIn: false
+        loggedIn: false,
+        seletcedPanel: 'order_info'
     }
 
+    setTimeout(function () {
+        spinner.style.display = 'none';
+        setTimeout(function () {
+            deliveryDetails.style.cssText = `display: block`;
+            loginBtnDiv.style.cssText = `display: block`;
+        }, 500);
+    }, 1500);
+
     window.addEventListener('resize', function () {
-        loginBtnDiv.style.position = 'static';
-        changeLoginButtonProperty();
+        if (!state.loggedIn) {
+            loginBtnDiv.style.position = 'static';
+            changeLoginButtonProperty();
+        }
     });
 
     body.addEventListener('click', function () {
-        if (loginPanel.style.display === 'none' && loginBtn.style.display === 'none') {
+        if (loginPanel.style.display === 'none' && loginBtn.style.display === 'none' && !state.loggedIn) {
             loginBtn.style.display = 'block';
         }
     });
@@ -51,26 +59,38 @@ window.onload = function () {
     // show login panel with grey filter background when user clicks 'Driver Login'
     loginBtn.addEventListener('click', function () {
         loginPanel.style.display = "flex";
-        loginBtnState = loginBtn.style.display;
         if (loginBtn.style.display !== 'none') {
             loginBtn.style.display = 'none';
         }
     });
 
-    // submit and send request server to request token
+    // LOGIN. submit and send request server to request token
     subLogin.addEventListener('click', function (e) {
         e.preventDefault();
         const username = document.querySelector('#username').value;
         const password = document.querySelector('#password').value;
         if (document.querySelector('#keptLogin').checked && !username && !password) {
             console.log('logged in');
+            loginPanel.classList.add('animate__zoomOut');
+            deliveryDetails.style.display = 'none';
+            spinner.style.display = 'block';
+            setTimeout(function () {
+                loginPanel.style.display = 'none';
+                loginPanel.classList.remove('animate__zoomOut');
+                setTimeout(function () {
+                    spinner.style.display = 'none';
+                    deliveryDetails.style.display = 'block';
+                    navigation.style.display = 'grid';
+                }, 500);
+            }, 1000);
             state.loggedIn = true;
+            loginBtn.style.display = 'none';
         }
     });
 
 
     // show sender details when users clicks 'sender'
-    document.querySelector('#sender').addEventListener('click', function () {
+    sender.addEventListener('click', function () {
         hideCardAndShowButton('receiverShow', 'senderShow', receiver, sender, this);
         this.children[1].children[3].style.display = 'block';
 
@@ -78,11 +98,37 @@ window.onload = function () {
     });
 
     // show receiver details when users clicks 'receiver'
-    document.querySelector('#receiver').addEventListener('click', function () {
+    receiver.addEventListener('click', function () {
         hideCardAndShowButton('senderShow', 'receiverShow', sender, receiver, this);
         this.children[1].children[3].style.display = 'block';
 
         changeLoginButtonProperty();
+    });
+
+    /* switch selected panel in navigation */
+    orderInfoBtn.addEventListener('click', function (e) {
+        e.stopPropagation();
+        state.selectedPanel = 'order_info';
+        orderInfoBtn.classList.add('selected');
+        instructionBtn.classList.remove('selected');
+        logoutBtn.classList.remove('selected');
+        console.log(state);
+    });
+
+    instructionBtn.addEventListener('click', function (e) {
+        e.stopPropagation();
+        state.selectedPanel = 'instruction';
+        orderInfoBtn.classList.remove('selected');
+        instructionBtn.classList.add('selected');
+        logoutBtn.classList.remove('selected');
+        console.log(state);
+    });
+
+    logoutBtn.addEventListener('click', function (e) {
+        e.stopPropagation();
+        state.loggedIn = false;
+        navigation.style.display = 'none';
+        loginBtn.style.cssText = `display: block`;
     });
 
     function hideCardAndShowButton(updateStateTo, currentObjState, partyToHide, partyToShow, jsNode) {
@@ -115,11 +161,11 @@ window.onload = function () {
             return;
         }
 
-        if (appContents.clientHeight >= (window.innerHeight - loginBtnDiv.clientHeight)) {
+        if (deliveryDetails.clientHeight >= (window.innerHeight - loginBtnDiv.clientHeight)) {
             loginBtnDiv.style.position = 'static';
         }
 
-        if (appContents.clientHeight < (window.innerHeight - loginBtnDiv.clientHeight)) {
+        if (deliveryDetails.clientHeight < (window.innerHeight - loginBtnDiv.clientHeight)) {
             loginBtnDiv.style.position = 'absolute';
         }
     }

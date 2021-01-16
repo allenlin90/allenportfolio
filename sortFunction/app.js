eeea4caf7ba5ad51f6ebfcf5f3fb821c00080bbd
allenlin90 <ht016746@gmail.com>
2021-01-16 13:39:54 +0700

    update sort function transition animation

--- sortFunction/app.js
+++ sortFunction/app.js
@@ -1,12 +1,14 @@
 window.onload = function () {
     const body = document.querySelector('body');
     const spinner = document.querySelector('.spinner');
+    const contentPanel = document.querySelector('.delivery');
     const sender = document.querySelector('#sender');
     const receiver = document.querySelector('#receiver');
     const collapse = document.querySelector('#collapse-btn');
     const loginPanel = document.querySelector('.login_panel');
     const loginForm = document.querySelector('.login_content');
     const subLogin = document.querySelector('#submitLogin');
+    const orderBody = document.querySelector('#order_body');
     const deliveryDetails = document.querySelector('#delivery_details');
     const loginBtnDiv = document.querySelector('.login_button');
     const loginBtn = document.querySelector('#driver-login');
@@ -20,7 +22,9 @@ window.onload = function () {
         senderShow: true,
         receiverShow: true,
         loggedIn: false,
-        selectedPanel: ''
+        selectedPanel: '',
+        deliveryImageHeight: 0,
+        deliveryImageWidth: 0
     }
 
     setTimeout(function () {
@@ -54,6 +58,7 @@ window.onload = function () {
             }, 1000);
             loginBtnDiv.style.display = 'block';
         }
+        freezeScreen();
     });
 
     // show login panel with grey filter background when user clicks 'Driver Login'
@@ -62,6 +67,7 @@ window.onload = function () {
         if (loginBtnDiv.style.display !== 'none') {
             loginBtnDiv.style.display = 'none';
         }
+        freezeScreen();
     });
 
     // LOGIN. submit and send request server to request token
@@ -70,6 +76,7 @@ window.onload = function () {
         const username = document.querySelector('#username').value;
         const password = document.querySelector('#password').value;
         if (document.querySelector('#keptLogin').checked && !username && !password) {
+            freezeScreen();
             console.log('logged in');
             loginPanel.classList.add('animate__zoomOut');
             deliveryDetails.style.display = 'none';
@@ -94,23 +101,36 @@ window.onload = function () {
 
     // show sender details when users clicks 'sender'
     sender.addEventListener('click', function () {
-        hideCardAndShowButton('receiverShow', 'senderShow', receiver, sender, this);
-        this.children[1].children[3].style.display = 'block';
-
-        changeLoginButtonProperty();
+        if (state.senderShow && state.receiverShow) {
+            freezeScreen();
+            hideCardAndShowButton('receiverShow', 'senderShow', receiver, sender, this);
+            this.children[1].children[3].style.display = 'block';
+            if (state.loggedIn) {
+                rerenderBottomBtn(navigation);
+            } else {
+                rerenderBottomBtn(loginBtnDiv);
+            }
+        }
     });
 
     // show receiver details when users clicks 'receiver'
     receiver.addEventListener('click', function () {
-        hideCardAndShowButton('senderShow', 'receiverShow', sender, receiver, this);
-        this.children[1].children[3].style.display = 'block';
-
-        changeLoginButtonProperty();
+        if (state.senderShow && state.receiverShow) {
+            freezeScreen();
+            hideCardAndShowButton('senderShow', 'receiverShow', sender, receiver, this);
+            this.children[1].children[3].style.display = 'block';
+            if (state.loggedIn) {
+                rerenderBottomBtn(navigation);
+            } else {
+                rerenderBottomBtn(loginBtnDiv);
+            }
+        }
     });
 
     /* switch selected panel in navigation */
     orderInfoBtn.addEventListener('click', function (e) {
         e.stopPropagation();
+        freezeScreen();
         console.log(state);
         if (state.selectedPanel !== 'order_info') {
             state.selectedPanel = 'order_info';
@@ -127,6 +147,7 @@ window.onload = function () {
 
     instructionBtn.addEventListener('click', function (e) {
         e.stopPropagation();
+        freezeScreen();
         state.selectedPanel = 'instruction';
         console.log(state);
         orderInfoBtn.classList.remove('selected');
@@ -141,6 +162,7 @@ window.onload = function () {
 
     logoutBtn.addEventListener('click', function (e) {
         e.stopPropagation();
+        freezeScreen();
         state.loggedIn = false;
         deliveryDetails.style.display = 'none';
         spinner.style.display = 'block';
@@ -165,25 +187,61 @@ window.onload = function () {
     });
 
     function hideCardAndShowButton(updateStateTo, currentObjState, partyToHide, partyToShow, jsNode) {
+        freezeScreen();
         state[updateStateTo] = false;
         partyToHide.style.cssText = 'display: none';
         partyToShow.style.width = `100%`;
+        /* hide delivery image starts */
+        if (deliveryImage.height) {
+            state.deliveryImageHeight = deliveryImage.height;
+            state.deliveryImageWidth = deliveryImage.width;
+        }
+        deliveryImage.style.visibility = 'hidden';
+        climbingAnimation(state.deliveryImageHeight, orderBody);
+        setTimeout(function () {
+            deliveryImage.style.display = 'none';
+            deliveryImage.style.visibility = 'visible';
+        }, 1000);
+        /* hide delivery image ends */
 
         // show return button
         collapse.style.display = 'block';
         collapse.innerText = 'Return';
         collapse.addEventListener('click', function () {
             if (!state[updateStateTo] && state[currentObjState]) {
+                freezeScreen();
                 console.log(`${updateStateTo} is returned`)
                 state[updateStateTo] = true;
                 partyToHide.style.cssText = 'display: block';
                 partyToShow.style.width = '50%';
                 collapse.style.display = 'none';
                 jsNode.children[1].children[3].style.display = 'none';
+                /* show image starts */
+                if (state.loggedIn) {
+                    navigation.style.display = `none`;
+                } else {
+                    loginBtnDiv.style.display = `none`;
+                }
+                climbingAnimation(state.deliveryImageHeight, orderBody, false)
+                setTimeout(function () {
+                    deliveryImage.classList.add('animate__slideInLeft');
+                    deliveryImage.style.display = 'block';
+                    setTimeout(function () {
+                        deliveryImage.classList.remove('animate__slideInLeft');
+                    }, 1000);
+                    /* show image ends */
+
+                }, 1000);
+                if (state.loggedIn) {
+                    rerenderBottomBtn(navigation);
+                } else {
+                    rerenderBottomBtn(loginBtnDiv);
+                }
             }
 
             changeLoginButtonProperty();
         });
+        changeLoginButtonProperty();
     }
 
     function changeLoginButtonProperty() {
@@ -198,5 +256,40 @@ window.onload = function () {
         if (deliveryDetails.clientHeight < (window.innerHeight - loginBtnDiv.clientHeight)) {
             loginBtnDiv.style.position = 'absolute';
         }
+    }
+
+    function rerenderBottomBtn(obj) {
+        obj.style.display = `none`;
+        obj.classList.add('animate__slideInUp');
+        setTimeout(function () {
+            obj.style.display = `grid`;
+            setTimeout(function () {
+                obj.classList.remove('animate__slideInUp');
+            }, 1000);
+        }, 1000);
+    }
+
+    function climbingAnimation(objSlideOutUpHeight, objClimbUp, rise = true, duration = 1000) {
+        if (!objSlideOutUpHeight) {
+            return;
+        }
+        let direction = '';
+        rise ? direction = `-` : direction = `+`;
+        const objHeight = objSlideOutUpHeight;
+        objClimbUp.style.position = `relative`;
+        objClimbUp.style.transition = `linear ${duration}ms`;
+        objClimbUp.style.transform = `translateY(${direction}${objHeight}px)`;
+        setTimeout(function () {
+            objClimbUp.style.position = `static`;
+            objClimbUp.style.transition = ``;
+            objClimbUp.style.transform = ``;
+        }, duration);
+    }
+
+    function freezeScreen(duration = 1000) {
+        body.classList.add(`freeze`);
+        setTimeout(function () {
+            body.classList.remove(`freeze`);
+        }, duration);
     }
 }

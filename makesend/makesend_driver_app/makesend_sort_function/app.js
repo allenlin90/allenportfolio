204e7df3ea35a538c2b2116b1d973fe8b573d4fa
allenlin90 <ht016746@gmail.com>
2021-05-14 13:37:33 +0700

    update MS driver sorting feature

--- makesend/makesend_driver_app/makesend_sort_function/app.js
+++ makesend/makesend_driver_app/makesend_sort_function/app.js
@@ -1,8 +1,12 @@
-window.onload = async function () {
+document.querySelector('.error_message__container__btns--close_btn').onclick = closeErrorIdList;
+document.querySelector('.error_message__container__btns--copy_btn').onclick = copyAllErrorIds();
+
+window.onload = async function testRun() {
     const state = {
         reloadTime: 0,
         data: null,
         selected: null,
+        errorIds: [],
         spinner: `
         <div class="spinner">
             <div class="spinner__ring spinner-border text-warning" role="status">
@@ -11,7 +15,7 @@ window.onload = async function () {
             <p class="spinner__text">Loading...</p>
         </div>`,
     }
-    setInputDOMs(state);
+
     // let parcelList = localStorage.getItem('parcelList'); // delete when deploy
     // state.data = JSON.parse(parcelList); // delete when deploy
     // const lastUpdate = Date.parse(localStorage.getItem('lastUpdate'));
@@ -29,49 +33,76 @@ window.onload = async function () {
         await timeoutReload(state.reloadTime, state);
     }
 
-    console.log(state.data);
+    setupAllDOMs(state);
+
     if (state.data.parcels.count) {
-        createOptions(state.data);
+        createOptions(state);
         renderResult(state);
     }
+    console.log(state);
 }
 
-function createOptions(data) {
-    const { parcels, shipments, accounts } = data;
+function createOptions(state) {
+    const { parcels, shipments, accounts } = state.data;
     const datalist = document.querySelector('#destinations');
     let optionsHTML = Object.entries(parcels).reduce((options, parcel) => {
-        console.log(parcel);
-        options += `
-        <option value='${parcel[0]}${String.fromCharCode(8218)} ${parcel[1].receiver_name}${String.fromCharCode(8218)} ${parcel[1].receiver_phone}${String.fromCharCode(8218)} ${parcel[1].car_plate}${String.fromCharCode(8218)} ${parcel[1].assigned_driver}${String.fromCharCode(8218)} ${shipments[parcel[0]]['dropAddress']}${String.fromCharCode(8218)} ${parcel[1].size}${String.fromCharCode(8218)} ${parcel[1].temp}'></option>
-        `;
-        return options;
+        if (!('#REF!' in shipments)) {
+            options += `
+            <option value='${parcel[0]}${String.fromCharCode(8218)} ${parcel[1].receiver_name}${String.fromCharCode(8218)} ${parcel[1].receiver_phone}${String.fromCharCode(8218)} ${parcel[1].car_plate}${String.fromCharCode(8218)} ${parcel[1].assigned_driver}${String.fromCharCode(8218)} ${shipments[parcel[0]]['dropAddress']}${String.fromCharCode(8218)} ${parcel[1].size}${String.fromCharCode(8218)} ${parcel[1].temp}'></option>
+            `;
+            return options;
+        } else {
+            state.errorIds.push(parcel[0]);
+        }
     }, '');
     datalist.innerHTML = optionsHTML;
 }
 
 function renderResult(data) {
     if (data.selected) {
+        const sizes = sizeList();
         const searchResult = document.querySelector('#sorting__search_result');
-        const trackingId = data.selected[0];
-        const receiverName = data.selected[1];
-        const receiverPhone = data.selected[2];
-        const carPlate = data.selected[3];
-        const assignedDriver = data.selected[4];
-        const address = data.selected[5];
-        const size = data.selected[6];
-        const temp = parseInt(data.selected[7]);
+        const trackingId = data.selected[0].trim();
+        const receiverName = data.selected[1].trim();
+        const receiverPhone = data.selected[2].trim();
+        const carPlate = data.selected[3].trim();
+        const assignedDriver = data.selected[4].trim();
+        const address = data.selected[5].trim();
+        const size = data.selected[6].trim();
+        const temp = parseInt(data.selected[7].trim());
+        const optionTags = sizes.reduce((tags, item, index) => {
+            if (item.toLowerCase().trim() === size.toLowerCase().trim()) {
+                tags += `<option selected value="${item}">${item}</option>`;
+            } else {
+                tags += `<option value="${item}">${item}</option>`;
+            }
+            return tags;
+        }, '');
         searchResult.innerHTML = `
         <div id="sorting__search_result--list">
             <p>Receiver Name: ${receiverName}</p>
-            <h3>Car Plate: ${carPlate} ${assignedDriver}</h3>
             <p>Receiver Phone: ${receiverPhone}</p>
-            <p>Address: <a href="https://www.google.com/maps/search/?q=${encodeURI(address)}">${address}</a></p>
-            <p>Tracking ID: ${trackingId}</p>
-            <p>Size: ${size}</p>
-            <p style="color: ${temp ? 'blue' : 'black'};">Temperature: ${temp ? 'Yes' : 'No'}</p>
+            <h3>Car Plate: ${carPlate} ${assignedDriver}</h3>
+            <hr>
+            <div id="size_selector">
+                <label for="size_select">Size:</label>
+                <select id="size_select"> 
+                    ${optionTags}
+                </select>
+                <div id="select_loader">
+                    <div class="spinner-border text-secondary" role="status" style="display: none;">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p class="${temp ? 'temp_control' : ''}">Temperature: ${temp ? 'Yes' : 'No'}</p>
+            <p>Address: <a href="https://www.google.com/maps/search/?q=${encodeURI(address)}" target="_blank">${address}</a></p>
+            <p>Tracking ID: <a href="https://app.makesend.asia/tracking?t=${trackingId}" target="_blank">${trackingId}</a></p>
             <!-- <button class="btn btn-warning">Transfer</button> -->
         </div>
         `;
+        document.querySelector('#size_selector').onchange = updateSize(trackingId, size);
         // confirmTransfer(data);
     }
 }
@@ -95,7 +126,7 @@ async function timeoutReload(reloadCount, state) {
         state.data = data;
         clearTimeout(timer);
         searchResult.innerHTML = `
-            <h1>Choose a parcel from the list </h1>
+            <h1 style="margin: 1rem;">Choose a parcel from the list </h1>
         `;
     }
 }
@@ -175,6 +206,18 @@ async function fetchData() {
     return list;
 }
 
+function setupAllDOMs(state) {
+    document.querySelector('.error_message__container__btns--close_btn').onclick = closeErrorIdList;
+    document.querySelector('.error_message__container__btns--copy_btn').onclick = copyAllErrorIds(state);
+    document.querySelector('#sorting__form').onsubmit = function (e) { e.preventDefault() };
+    if (state.errorIds.length) {
+        const errorListBtn = document.querySelector('#sorting__form button');
+        errorListBtn.style.display = 'inline-block';
+        errorListBtn.onclick = openErrorList;
+    }
+    setInputDOMs(state);
+}
+
 function setInputDOMs(state) {
     const listInput = document.querySelector('#sorting__form input');
     listInput.onchange = function (event) {
@@ -210,4 +253,69 @@ function confirmTransfer(state = null) {
             }, 3000);
         }
     }
+}
+
+function closeErrorIdList(event) {
+    document.querySelector('.error_message').style.display = 'none';
+}
+
+function copyAllErrorIds(state = null) {
+    if (state) {
+        return async function (event) {
+            console.log(state.errorIds);
+            await navigator.clipboard.writeText(state.errorIds.join(String.fromCharCode(10)));
+        }
+    }
+}
+
+function openErrorList(event) {
+    document.querySelector('.error_message').style.display = 'block';
+}
+
+function sizeList() {
+    return [
+        'env',
+        'polym',
+        'polyl',
+        's40',
+        's60',
+        's80',
+        's100',
+        's120',
+        's140',
+        's160',
+        's180',
+        's200',
+    ];
+}
+
+function updateSize(shipmentId = '') {
+    if (shipmentId) {
+        return async function (event) {
+            const selectLoader = document.querySelector('#select_loader');
+            selectLoader.style.display = 'inline-block';
+            const size = document.querySelector('#size_selector select').value;
+            const options = {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    trackingID: shipmentId,
+                    // size
+                }),
+            }
+            const res = await fetch('https://api.airportels.asia/api/google/makesend/shipment/changeSize', options)
+                .then(res => res.json()).then(data => data).catch(err => err);
+            if (res.resCode === 200) {
+                console.log(res);
+                selectLoader.innerHTML = `<i style="color: green" class="fas fa-check"></i>`;
+            } else {
+                selectLoader.innerHTML = `<i style="color: red" class="fas fa-times-circle"></i>`;
+                console.log('Server Error: ' + res.message);
+                alert('Server Error: ' + res.message);
+            }
+        }
+    }
+    console.warn(`shipmentId or size isn't given to 'updateSize'`);
 }

b840e239597b6d9be7a40ba79d974eeae1593ec3
allenlin90 <ht016746@gmail.com>
2021-09-16 16:25:17 +0700

    add msgo dropoff service

---
+++ makesend/makesend_msgo_dropoff/app.js
@@ -0,0 +1,14 @@
+// window.onload = async function () {
+//   const orderList = document.querySelector('#order_list');
+//   const parcelList = document.querySelector('#parcel_list');
+
+//   parcelList.style.display = 'none';
+
+//   const rows = document.querySelectorAll('tbody tr');
+//   rows.forEach((row) => {
+//     row.onclick = (e) => {
+//       orderList.style.display = 'none';
+//       parcelList.style.display = 'block';
+//     };
+//   });
+// };

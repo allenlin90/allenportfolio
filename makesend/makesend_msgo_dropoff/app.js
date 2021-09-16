e0a773ef869c7cb0e26e806ac5869a3dfa3d7b07
allenlin90 <ht016746@gmail.com>
2021-09-16 16:29:50 +0700

    enable Javascript code for makesend dropoff service

--- makesend/makesend_msgo_dropoff/app.js
+++ makesend/makesend_msgo_dropoff/app.js
@@ -1,14 +1,14 @@
-// window.onload = async function () {
-//   const orderList = document.querySelector('#order_list');
-//   const parcelList = document.querySelector('#parcel_list');
+window.onload = async function () {
+  const orderList = document.querySelector('#order_list');
+  const parcelList = document.querySelector('#parcel_list');
 
-//   parcelList.style.display = 'none';
+  parcelList.style.display = 'none';
 
-//   const rows = document.querySelectorAll('tbody tr');
-//   rows.forEach((row) => {
-//     row.onclick = (e) => {
-//       orderList.style.display = 'none';
-//       parcelList.style.display = 'block';
-//     };
-//   });
-// };
+  const rows = document.querySelectorAll('tbody tr');
+  rows.forEach((row) => {
+    row.onclick = (e) => {
+      orderList.style.display = 'none';
+      parcelList.style.display = 'block';
+    };
+  });
+};
